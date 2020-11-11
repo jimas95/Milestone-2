5ec371952d096d3ee66ec304d7a7966338ce99d2
chamzasd <chamzas95@gmail.com>
2020-11-11 14:29:37 -0600

    first commit created functions file created main file

---
+++ functions.py
@@ -0,0 +1,10 @@
+import modern_robotics as mr
+import math
+import numpy as np
+
+
+
+def test():
+    print("Test")
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+import functions
+
+
+
+
+def main():
+
+    print("Hello")
+    functions.test()
+
+
+
+if __name__ == "__main__":
+    main()

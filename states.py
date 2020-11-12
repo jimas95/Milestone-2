8255309686c310dff5a59a8f87452a731ed6df31
chamzasd <chamzas95@gmail.com>
2020-11-11 22:23:07 -0600

    used functions

--- states.py
+++ states.py
@@ -1,132 +1,53 @@
 import math
 import numpy as np
+import functions
 
 
-# go near the object
-start = np.array( [[1, 0, 0, 0  ],
-                    [0, 1, 0, 0  ],
-                    [0, 0, 1, 0.5],
-                    [0, 0, 0, 1  ]])
 
-
-angle = 3*math.pi/4
-end = np.array( [  [math.cos(angle)     , 0     , math.sin(angle)   , 0.85  ],
-                    [0                  , 1     , 0                 , 0     ],
-                    [-math.sin(angle)   , 0     , math.cos(angle)   , 0.15  ],
-                    [0                  , 0     , 0                 , 1     ]])
-
-                    
 Tf = 5
 N = 100
 gripper = 0
+angle = 3*math.pi/4
+
+# go near the object
+start = functions.trasl(0,0,0.5)
+end = functions.rot_y(angle,0.85,0,0.15)
 state1 = [start,end,Tf,N,gripper]
 
 
 # go at grasping position
-angle = 3*math.pi/4
-start =np.array([  [math.cos(angle)     , 0     , math.sin(angle)   , 0.85  ],
-                    [0                  , 1     , 0                 , 0     ],
-                    [-math.sin(angle)   , 0     , math.cos(angle)   , 0.15  ],
-                    [0                  , 0     , 0                 , 1     ]])
-
-
-angle = 3*math.pi/4
-end = np.array( [   [math.cos(angle)    , 0     , math.sin(angle)   , 1.00  ],
-                    [0                  , 1     , 0                 , 0     ],
-                    [-math.sin(angle)   , 0     , math.cos(angle)   , 0.0   ],
-                    [0                  , 0     , 0                 , 1     ]])
-
-                    
-Tf = 5
-N = 100
-gripper = 0
+start = end
+end = functions.rot_y(angle,1,0,0)
 state2 = [start,end,Tf,N,gripper]
 
 
 
 #pick the object
-angle = 3*math.pi/4
-start=np.array( [   [math.cos(angle)    , 0     , math.sin(angle)   , 1.00  ],
-                    [0                  , 1     , 0                 , 0     ],
-                    [-math.sin(angle)   , 0     , math.cos(angle)   , 0.0   ],
-                    [0                  , 0     , 0                 , 1     ]])
-
-
-angle = 3*math.pi/4
-end = np.array( [   [math.cos(angle)    , 0     , math.sin(angle)   , 1.00  ],
-                    [0                  , 1     , 0                 , 0     ],
-                    [-math.sin(angle)   , 0     , math.cos(angle)   , 0.0   ],
-                    [0                  , 0     , 0                 , 1     ]])
-
-                    
-Tf = 5
-N = 100
+start=end
+end = functions.rot_y(angle,1,0,0)
 gripper = 1
 state3 = [start,end,Tf,N,gripper]
 
 
 #go up
-angle = 3*math.pi/4
-start=np.array( [   [math.cos(angle)    , 0     , math.sin(angle)   , 1.00  ],
-                    [0                  , 1     , 0                 , 0     ],
-                    [-math.sin(angle)   , 0     , math.cos(angle)   , 0.0   ],
-                    [0                  , 0     , 0                 , 1     ]])
-
-
-angle = 3*math.pi/4
-end = np.array( [   [math.cos(angle)    , 0     , math.sin(angle)   , 1.00  ],
-                    [0                  , 1     , 0                 , 0     ],
-                    [-math.sin(angle)   , 0     , math.cos(angle)   , 0.2   ],
-                    [0                  , 0     , 0                 , 1     ]])
-
-                    
+start=end
+end = functions.rot_y(angle,1,0,0.2)               
 Tf = 1
-N = 100
 gripper = 1
 state4 = [start,end,Tf,N,gripper]
 
 
-#go to dropping location
-# angle = 3*math.pi/4
-# start=np.array( [   [math.cos(angle)    , 0     , math.sin(angle)   , 1.00  ],
-#                     [0                  , 1     , 0                 , 0     ],
-#                     [-math.sin(angle)   , 0     , math.cos(angle)   , 0.2   ],
-#                     [0                  , 0     , 0                 , 1     ]])
-
-
-# angle = math.pi/2
-# end  =np.array( [   [math.cos(angle)    , 0     , math.sin(angle)   , 1.00  ],
-#                     [0                  , 1     , 0                 , 0     ],
-#                     [-math.sin(angle)   , 0     , math.cos(angle)   , 0.2   ],
-#                     [0                  , 0     , 0                 , 1     ]])
-                    
-# Tf = 1
-# N = 100
-# gripper = 1
-# state5 = [start,end,Tf,N,gripper]
-
-
 
 
 #go to dropping location
-angle = 3*math.pi/4
-start=np.array( [   [math.cos(angle)    , 0     , math.sin(angle)   , 1.00  ],
-                    [0                  , 1     , 0                 , 0     ],
-                    [-math.sin(angle)   , 0     , math.cos(angle)   , 0.2   ],
-                    [0                  , 0     , 0                 , 1     ]])
-
-
-
+start=end
 angle = -math.pi/2
-end2 =np.array( [   [math.cos(angle)    , -math.sin(angle)  , 0   , 0  ],
-                    [math.sin(angle)    , math.cos(angle)   , 0   , 0  ],
-                    [0                  , 0                 , 1   , 0  ],
-                    [0                  , 0                 , 0   , 1  ]])
+rotation = functions.rot_z(angle,0,0,0)
 
 
-end = np.matmul(end2,start)
-Tf = 1
-N = 100
+
+end = np.matmul(rotation,start)
+Tf = 5
 gripper = 1
 state5 = [start,end,Tf,N,gripper]
 
@@ -138,18 +59,31 @@ start= end
 
 
 angle = -math.pi/2
-end2    = np.array([[1, 0, 0, 0   ],
-                    [0, 1, 0, 0   ],
-                    [0, 0, 1, -0.2],
-                    [0, 0, 0, 1]])
+rotation    = functions.trasl(0,0,-0.2)
 
 
 Tf = 1
-N = 100
 gripper = 1
-end = np.matmul(end2,start)
+end = np.matmul(rotation,start)
 state6 = [start,end,Tf,N,gripper]
 
+#open gripper
+start= end
+Tf = 1
+gripper = 0
+state7 = [start,end,Tf,N,gripper]
 
-states = [state1,state2,state3,state4,state5,state6]
-# states = [state5,state6]
+#go up
+start = end
+rotation    = functions.trasl(0,0,0.2)
+end = np.matmul(rotation,start)
+
+state8 = [start,end,Tf,N,gripper]
+
+# go to home position
+tf = 5
+start = end
+end = functions.trasl(0,0,0.5)
+
+state9 = [start,end,Tf,N,gripper]
+states = [state1,state2,state3,state4,state5,state6,state7,state8,state9]
